e854bbba490708f7615f944da3a6b6592b03f5ba
johnrand <johnrandy0652@gmail.com>
2024-02-21 00:59:38 +0300

    Initial commit Swapping values Bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+ * swap2values - A function that swaps two values
+ * @x: Pointer to First value
+ * @y: Pointer to Second value
+ *
+ * Returns: Void
+ */
+
+void swap2values(int *x, int *y)
+{
+	*y = *x ^ *y;
+	*x = *x ^ *y;
+	*y = *x ^ *y;
+}
+
+
+/**
  * bubble_sort - A function that sorts the array of size
  * @array: A pointer to the array to sort
  * @size: The size of the array
